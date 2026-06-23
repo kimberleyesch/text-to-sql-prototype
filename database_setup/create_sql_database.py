267782158fb216fa47ff8317fa2f6d4ebb98f43b
Kimberley <kimberley.esch@gmail.com>
2026-06-23 14:08:53 +0200

    Add database setup and gitignore

---
+++ database_setup/create_sql_database.py
@@ -0,0 +1,162 @@
+import sqlite3
+import test_data_generator
+import pandas as pd
+from pathlib import Path
+
+
+# -----------------------------------------------------------------------------------------
+# Constants
+
+DATABASE_PATH = Path("business.db")
+
+INSERT_ORDER = [
+    "customers",
+    "categories",
+    "products",
+    "orders",
+    "order_items",
+]
+
+
+CREATE_TABLES_SQL = """
+    CREATE TABLE customers (
+        customer_id INTEGER PRIMARY KEY,
+        company_name TEXT NOT NULL,
+        industry TEXT,
+        company_size TEXT
+            CHECK (company_size IN ('small', 'medium', 'large')),
+        country TEXT,
+        city TEXT,
+        acquisition_date TEXT NOT NULL DEFAULT CURRENT_DATE
+            CHECK (acquisition_date = date(acquisition_date)
+                    AND date(acquisition_date) IS NOT NULL),
+        is_active INTEGER NOT NULL DEFAULT 1
+            CHECK (is_active IN (1, 0))
+    );
+
+    CREATE TABLE categories (
+        category_id INTEGER PRIMARY KEY,
+        category_name TEXT NOT NULL UNIQUE
+    );
+
+    CREATE TABLE products (
+        product_id INTEGER PRIMARY KEY,
+        product_name TEXT NOT NULL UNIQUE,
+        category_id INTEGER NOT NULL,
+        current_unit_price_cents INTEGER NOT NULL
+            CHECK (current_unit_price_cents >= 0),
+        current_unit_cost_cents INTEGER NOT NULL
+            CHECK (current_unit_cost_cents >= 0),
+            
+        FOREIGN KEY (category_id)
+            REFERENCES categories(category_id)
+    );
+
+    CREATE TABLE orders (
+        order_id INTEGER PRIMARY KEY,
+        customer_id INTEGER NOT NULL,
+        order_date TEXT NOT NULL DEFAULT CURRENT_DATE
+            CHECK (order_date = date(order_date)
+                    AND date(order_date) IS NOT NULL),
+        order_status TEXT NOT NULL
+            CHECK (order_status IN ('pending', 'completed', 'cancelled')),
+            
+        FOREIGN KEY (customer_id)
+            REFERENCES customers(customer_id)
+    );
+
+    CREATE TABLE order_items (
+        order_item_id INTEGER PRIMARY KEY,
+        order_id INTEGER NOT NULL,
+        product_id INTEGER NOT NULL,
+        
+        order_unit_price_cents INTEGER NOT NULL
+            CHECK (order_unit_price_cents >= 0),
+        order_unit_cost_cents INTEGER NOT NULL
+            CHECK (order_unit_cost_cents >= 0),
+
+        quantity INTEGER NOT NULL
+            CHECK (quantity > 0),
+        
+        discount_percent INTEGER NOT NULL
+            CHECK (discount_percent >= 0 AND discount_percent <= 100),
+            
+        FOREIGN KEY (order_id)
+            REFERENCES orders(order_id),
+        FOREIGN KEY (product_id)
+            REFERENCES products(product_id)
+    );
+
+
+"""
+
+def get_available_database_path(base_path):
+    if not base_path.exists():
+        return base_path
+    
+    number = 1
+
+    while True:
+        new_path = base_path.with_name(f"{base_path.stem}_{number}{base_path.suffix}")
+
+        if not new_path.exists():
+            return new_path
+        
+        number += 1
+
+
+def create_database_tables(connection):
+    connection.executescript(CREATE_TABLES_SQL)
+
+
+def prepare_dataframe(dataframe):
+    dataframe = dataframe.copy()
+
+    date_columns = ["acquisition_date", "order_date"]
+    
+    for column in date_columns:
+        if column in dataframe.columns:
+            dataframe[column] = (pd.to_datetime(dataframe[column]).dt.strftime("%Y-%m-%d"))
+
+    return dataframe
+
+
+def insert_test_data(connection):
+    test_data = test_data_generator.create_test_data()
+
+    for table_name in INSERT_ORDER:
+        dataframe = prepare_dataframe(test_data[table_name])
+
+        dataframe.to_sql(
+            name=table_name,
+            con=connection,
+            if_exists="append",
+            index=False
+        )
+
+        print(f"{len(dataframe)} datarows were successfully transfered to {table_name}.")
+
+
+def main():
+    database_path = get_available_database_path(DATABASE_PATH)
+
+    try:
+        with sqlite3.connect(database_path) as connection:
+            connection.execute("PRAGMA foreign_keys = ON")
+
+            create_database_tables(connection)
+            insert_test_data(connection)
+
+            connection.commit()
+    
+    except Exception:
+        if database_path.exists():
+            database_path.unlink()
+
+        raise
+
+    print(f"\nDatabase was successfully created: {database_path.resolve()}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ database_setup/test_data_generator.py
@@ -0,0 +1,242 @@
+import random
+import pandas as pd
+from faker import Faker
+
+
+# -----------------------------------------------------------------------------------------
+# Constants
+
+#TODO ------------ FUNTKIONIEREN DIe überhaupt als Begrenzung? ich glaube nicht
+DEFAULT_CUSTOMER_COUNT = 20
+DEFAULT_PRODUCT_COUNT = 230  # max: 230
+DEFAULT_ORDERS_COUNT = 100
+MAX_ITEMS_PER_ORDER= 15
+
+TABLE_SCHEMAS = {"customers": ["customer_id", "company_name", "industry", "company_size", "country", "city", "acquisition_date", "is_active"],
+                "categories": ["category_id", "category_name"],
+                "products": ["product_id", "product_name", "category_id", "current_unit_price_cents", "current_unit_cost_cents"],
+                "orders": ["order_id", "customer_id", "order_date", "order_status"],
+                "order_items": ["order_item_id","order_id", "product_id", "order_unit_price_cents", "order_unit_cost_cents", "quantity", "discount_percent"],
+                }
+
+INDUSTRY_SECTORS = [
+    "Industrial Automation",
+    "Robotics",
+    "Mechanical Engineering",
+    "Electrical Engineering",
+    "Electronics Manufacturing",
+    "Semiconductor Manufacturing",
+    "Automotive Technology",
+    "Electric Vehicles",
+    "Aerospace and Defence",
+    "Railway Technology",
+    "Medical Technology",
+    "Laboratory Equiptment",
+    "Renewable Energy",
+    "Energy Storage",
+    "Telecommunications",
+    "Data Center Infrastructure",
+    "Building Automation",
+    "Agricultural Technology",
+    "Logistics Automation",
+    "Advanced Manufacturing"
+]
+
+COMPANY_SIZES = ["small", "medium", "large"]
+
+ORDER_STATUS = ["completed", "pending", "cancelled"]
+
+LOCATION_URL = "https://raw.githubusercontent.com/datasets/world-cities/main/data/world-cities.csv"
+
+PRODUCTS_URL = "https://huggingface.co/api/resolve-cache/datasets/crawlfeeds/HomeDepot-Smart-Home-Dataset/8d71a34c8d5f9210a0d57a3b62178072d6f069c0/crawlfeeds_homedepot__limit-2000000_category_1-smart-home_20260409_191051.csv?download=true"
+
+
+# -----------------------------------------------------------------------------------------
+# Read data from urls
+
+def read_location_data():
+    required_columns = {"name", "country"}
+
+    try:
+        location_data = pd.read_csv(LOCATION_URL)
+    except Exception as error:
+        raise RuntimeError("Cities and Countries could not be read from file.") from error
+
+    if not required_columns.issubset(location_data.columns) or (len(location_data) < DEFAULT_CUSTOMER_COUNT):
+        raise ValueError("Cities and Countries file data differs from the expected.")
+    
+    return location_data
+
+def read_product_data():
+    required_columns = {"product_name", "price", "category_4"}
+    
+    try:
+        product_data = pd.read_csv(PRODUCTS_URL)
+    except Exception as error:
+        raise RuntimeError("Products could not be read from file.") from error
+
+    if not required_columns.issubset(product_data.columns) or (len(product_data) < DEFAULT_PRODUCT_COUNT):
+        raise ValueError("Product file data differs from the expected.")
+
+    return product_data
+
+
+# -----------------------------------------------------------------------------------------
+# create test data
+
+# creats customer test data
+# return: test data in pandas data frame
+def create_customers():
+
+    fake = Faker("en_US")
+
+    # read cities and countries
+    location_data = read_location_data()
+
+    # choose random location data
+    random_locations = location_data.sample(n=DEFAULT_CUSTOMER_COUNT)
+    cities = random_locations["name"].tolist()
+    countries = random_locations["country"].tolist()
+
+    
+    # create customer table
+    customers = pd.DataFrame({
+        "customer_id": range(1, DEFAULT_CUSTOMER_COUNT+1),
+        "company_name": [fake.unique.company() for _ in range(DEFAULT_CUSTOMER_COUNT)],
+        "industry": [random.choice(INDUSTRY_SECTORS) for _ in range(DEFAULT_CUSTOMER_COUNT)],
+        "company_size": random.choices(COMPANY_SIZES, k=DEFAULT_CUSTOMER_COUNT),
+        "country": countries,
+        "city": cities,
+        "acquisition_date": [fake.date_between() for _ in range(DEFAULT_CUSTOMER_COUNT)],    # defaults between -30 years and today
+        "is_active": random.choices(population=[1, 0], weights=[90, 10], k=DEFAULT_CUSTOMER_COUNT),
+    })
+
+    return customers
+
+
+
+# creats categories test data
+# return: test data in pandas data frame
+def create_categories():
+    product_data = read_product_data()
+
+    unique_categories = (product_data["category_4"].dropna().drop_duplicates().reset_index(drop=True))
+
+    # create categories table
+    categories = pd.DataFrame({
+        "category_id": range(1, len(unique_categories)+1),
+        "category_name": unique_categories,
+    })
+
+    return categories
+
+
+# creats products test data
+# return: test data in pandas data frame
+def create_products(categories):
+    product_data = read_product_data()
+
+    categories_mapping = dict(zip(categories["category_name"], categories["category_id"]))
+
+    prices_cents = (pd.to_numeric(product_data["price"], errors="coerce")*100).round()
+
+    product_prices = []
+    product_costs = []
+
+    for price in prices_cents:
+        if pd.isna(price):
+            price = random.randint(100, 100000)
+        else:
+            price = int(price)
+
+        product_prices.append(price)
+
+        product_costs.append(round(price*0.9))
+
+
+    # create products table
+    products = pd.DataFrame({
+        "product_id": range(1, len(product_data)+1),
+        "product_name": product_data["product_name"],
+        "category_id": product_data["category_4"].map(categories_mapping),
+        "current_unit_price_cents": product_prices,
+        "current_unit_cost_cents": product_costs,
+    })
+
+    return products
+
+
+# creats orders test data
+# return: test data in pandas data frame
+def create_orders(customers):
+    fake = Faker("en_US")
+
+    selected_customer_ids = [random.choice(customers["customer_id"]) for _ in range(DEFAULT_ORDERS_COUNT)]
+    acquisition_dates = dict(zip(customers["customer_id"], customers["acquisition_date"]))
+
+    # create orders table
+    orders = pd.DataFrame({
+        "order_id": range(1, DEFAULT_ORDERS_COUNT+1),
+        "customer_id": selected_customer_ids,
+        "order_date": [fake.date_between(start_date=acquisition_dates[customer_id], end_date="today") for customer_id in selected_customer_ids],
+        "order_status": random.choices(ORDER_STATUS, weights=[70, 10, 20], k=DEFAULT_ORDERS_COUNT),
+    })
+
+    return orders
+
+# creats order_items test data
+# return: test data in pandas data frame
+def create_order_items(orders, products):
+    order_items_data = []
+
+    for order_id in orders["order_id"]:
+
+        num_of_items = random.randint(1, MAX_ITEMS_PER_ORDER)
+
+        select_products = products.sample(n=num_of_items)
+
+        for _, product in select_products.iterrows():
+            order_items_data.append({
+                "order_item_id": len(order_items_data)+1,
+                "order_id": order_id,
+                "product_id": product["product_id"],
+                "order_unit_price_cents": product["current_unit_price_cents"],
+                "order_unit_cost_cents": product["current_unit_cost_cents"],
+                "quantity": random.randint(1, 10),
+                "discount_percent": random.choices(population=[0,5,10,15,20,30], weights=[60,15,10,5,5,5],k=1)[0],
+            })
+
+    order_items = pd.DataFrame(order_items_data)
+
+    return order_items
+
+
+# -----------------------------------------------------------------------------------------
+# create all test data
+def create_test_data():
+    customers = create_customers()
+    categories = create_categories()
+    products = create_products(categories)
+    orders = create_orders(customers)
+    order_items = create_order_items(orders, products)
+    
+    
+    return {
+        "customers": customers,
+        "categories": categories,
+        "products": products,
+        "orders": orders,
+        "order_items": order_items,
+    }
+
+
+def main():
+    test_data = create_test_data()
+
+    for table_name, dataframe in test_data.items():
+        print(f"\n--- {table_name} ---")
+        print(dataframe.head())
+        print(f"Rows: {len(dataframe)}")
+
+if __name__ == "__main__":
+    main()
