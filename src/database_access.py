fce0761e7ae2f6b77ef434def52423e6b0ff7487
Kimberley <kimberley.esch@gmail.com>
2026-06-29 10:50:11 +0200

    Add function to execute sql query

--- src/database_access.py
+++ src/database_access.py
@@ -1,8 +1,10 @@
 import sqlite3
 from pathlib import Path
 
+
 PROJ_ROOT = Path(__file__).resolve().parent.parent
 DATABASE_PATH = PROJ_ROOT / "database_setup" / "business.db"
+
 
 def get_connection():
     """Open and return connection to SQLite database."""
@@ -16,11 +18,12 @@ def get_connection():
     except sqlite3.Error as e:
         raise sqlite3.Error(f"Could not connect to the database: {e}")
 
+
 def get_schema():
     """Read and return database schema"""
 
-    connetion = get_connection()
-    cursor = connetion.cursor()
+    connection = get_connection()
+    cursor = connection.cursor()
 
     cursor.execute("""
                     SELECT sql
@@ -37,7 +40,29 @@ def get_schema():
         if row[0] is not None:
             statements.append(row[0])
     
-    connetion.close()
+    connection.close()
 
     schema = "\n\n".join(statements)
     return schema
+
+
+def execute_query(sql_query):
+    """Execute a SELECT query and return all result rows."""
+
+    if not sql_query.strip().lower().startswith("select"):
+        raise ValueError("Only SELECT queries are allowed.")
+        #TODO hier später noch hinzufügen welche Fragenummer das war
+    
+    connection = get_connection()
+
+    try:
+        cursor = connection.cursor()
+        cursor.execute(sql_query.strip())
+        results = cursor.fetchall()
+
+        connection.close()
+        return results
+    
+    except sqlite3.Error as e:
+        connection.close()
+        raise RuntimeError(f"SQL error occurred: {e}")
