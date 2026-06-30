e8d47304d50e24c6aaed6dd458fb497126d3c87c
Kimberley <kimberley.esch@gmail.com>
2026-06-30 11:25:49 +0200

    Import gemini 3.5 flash and run test

--- src/llm_client.py
+++ src/llm_client.py
@@ -1,5 +1,25 @@
-def generate_sql(prompt):
+import os
+from google import genai
+from dotenv import load_dotenv
+from pathlib import Path
 
-    sql_query = "SELECT company_name, company_size FROM customers WHERE company_size = 'medium'"
+"""GET API Key from .env"""
+PROJ_ROOT = Path(__file__).resolve().parent.parent
+dotenv_path = PROJ_ROOT.resolve()
+load_dotenv(dotenv_path=dotenv_path)
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+interaction = client.interactions.create(
+    model="gemini-3.5-flash",
+    input="How does one select all columns in a SQLite database from a customers table? Reply only with the SQL query"
+)
+print(interaction.output_text)
+
+# def generate_sql(prompt):T
+#     """Send prompt to LLM and return the generated SQL query"""
     
-    return sql_query
+#     sql_query = "SELECT company_name, company_size FROM customers WHERE company_size = 'medium'"
+    
+#     return sql_query
