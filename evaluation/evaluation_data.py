1b8a5606c2f0ba5f071d5de8f48dbb66ffef5e73
Kimberley <kimberley.esch@gmail.com>
2026-07-01 12:20:59 +0200

    Save test results to CSV

--- evaluation/evaluation_data.py
+++ evaluation/evaluation_data.py
@@ -3,10 +3,58 @@ from pathlib import Path
 
 EVALUATION_DIR = Path(__file__).resolve().parent
 QUESTIONS_PATH = EVALUATION_DIR / "test_questions.xlsx"
-# RESULTS_PATH = EVALUATION_DIR / "test_results.xlsx"
+RESULTS_PATH = EVALUATION_DIR / "results"
+
 
 def get_questions():
-    question_data = pd.read_excel(QUESTIONS_PATH)
-    questions = question_data["Test Questions"]
+    """Read test question and ID from Excel file."""
+    question_data = pd.read_excel(QUESTIONS_PATH, dtype={"ID": str})
 
-    return questions
+    required_columns = {"ID", "Test Questions"}
+
+    if not required_columns.issubset(question_data.columns):
+        raise ValueError(f"Excel file doesn't contain required columns: {required_columns}")
+
+    questions = question_data["Test Questions"]
+    question_ids = question_data["ID"].str.strip().to_list()
+
+    return questions, question_ids
+
+
+def save_result(question_id, column_names, results):
+    """Save the query result in new CSV file"""
+
+    RESULTS_PATH.mkdir(exist_ok=True)
+
+    df = pd.DataFrame(
+        results,
+        columns=column_names
+    )
+
+    path_name = get_next_result_path(question_id)
+
+    df.to_csv(path_name, index=False, sep=";")
+
+
+def get_next_result_path(id):
+    """Return the next available file path name for the question"""
+    counter = 1
+
+    path_name = RESULTS_PATH / f"{id}_result_{counter}.csv"
+
+    while path_name.exists():
+        counter += 1
+        path_name = RESULTS_PATH / f"{id}_result_{counter}.csv"
+        
+    return path_name
+
+
+def main():
+    id = 'Q01'
+    col = ['company_name', 'country']
+    #col.insert(0, "id")
+    list = [('Kirk-Hunter', 'Japan'), ('Nguyen-Burton', 'France'), ('Bush, Brown and Lawrence', 'China'), ('Williams-Lester', 'Chile'), ('Peterson Inc', 'China'), ('Klein, Schmitt and Johnson', 'Colombia'), ('Hancock-Carson', 'India'), ('Fitzpatrick-Walker', 'Canada'), ('Lambert Inc', 'United Kingdom'), ('Martin-Warren', 'Italy'), ('Love-Harris', 'India'), ('Perry-Thompson', 'Brazil')]
+    save_result(id, col, list)
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -3,23 +3,34 @@ from src.database_access import execute_query
 from src.prompt_builder import build_prompt
 from src.llm_client import generate_sql
 from evaluation.evaluation_data import get_questions
+from evaluation.evaluation_data import save_result
 
 def main():
-    question = "Show all medium sized companies"
+    """Run complete Text-to-SQL workflow
+
+    Read database schema and test questions, send prompt to LLM, receive SQL-queries,
+    execute queries in SQL and save results.
+    """
 
     schema = get_schema()
-    questions = get_questions()
-    question = questions[1]
-    prompt = build_prompt(question, schema)
-    sql_query = generate_sql(prompt)
-    column_names, results = execute_query(sql_query)
+    questions, question_ids = get_questions()
 
-    print(f"\nQuestion: {question}")
-    print("\n")
-    print(" | ".join(column_names))
+    for i in range(3):
+        question_id = question_ids[i]
+        question = questions[i]
+    # for question_id, question in zip(question_ids, questions):
 
-    for row in results:
-        print(row)
+        prompt = build_prompt(question, schema)
+        sql_query = generate_sql(prompt)
+        column_names, results = execute_query(sql_query)
+        save_result(question_id, column_names, results)
+
+        print(f"\nQuestion: {question}")
+        print("\n")
+        print(" | ".join(column_names))
+
+        for row in results:
+            print(row)
 
 if __name__ == "__main__":
     main()
