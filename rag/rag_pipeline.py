66c743c266cce91cbe25d1ed2f8bd2a3c1e64ca0
Kimberley <kimberley.esch@gmail.com>
2026-07-09 13:16:36 +0200

    Create RAG pipeline

---
+++ rag/rag_pipeline.py
@@ -0,0 +1,94 @@
+import sys
+import chromadb
+from google.genai import types
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parents[1] / "src"))
+
+RAG_DOKUS_DIR = Path(__file__).resolve().parent / "documents"
+CHROMADB_PATH = Path(__file__).resolve().parent / "chromaDB"
+CHROMADB_PATH.mkdir(exist_ok=True)
+
+
+def load_txt_documents(folder_path):
+    """Load all text files from the documents path."""
+
+    documents = []
+
+    for file in sorted(folder_path.glob("*.txt")):
+        with open(file, "r", encoding="utf-8") as f:
+            content = f.read().strip()
+
+            documents.append({
+                "source": file.name,
+                "content": content
+            })
+
+    return documents
+
+def create_embedding(client):
+    documents = load_txt_documents(RAG_DOKUS_DIR)
+    embedded_documents = []
+
+    for document in documents:
+        result = client.models.embed_content(
+            model="gemini-embedding-2",
+            contents=document["content"],
+            config=types.EmbedContentConfig(output_dimensionality=768)
+        )
+
+        embedding = result.embeddings[0].values
+
+        embedded_documents.append({
+            "source": document["source"],
+            "content": document["content"],
+            "embedding": embedding
+        })
+
+    return embedded_documents
+
+def save_embeddings_in_db(embedded_documents):
+    chroma_client = chromadb.PersistentClient(path=str(CHROMADB_PATH))
+
+    collection = chroma_client.get_or_create_collection(
+        name="rag-documents",
+        metadata={"hnsw:space": "cosine"}
+    )
+
+    ids = []
+    embeddings = []
+    metadatas = []
+    documents = []
+
+    for i, doc in enumerate(embedded_documents):
+        ids.append(f"id{i}")
+        embeddings.append(doc["embedding"])
+        metadatas.append({"source": doc["source"]})
+        documents.append(doc["content"])
+
+    collection.upsert(
+        ids=ids,
+        embeddings=embeddings,
+        metadatas=metadatas,
+        documents=documents
+    )
+
+    print(f"Saved documents: {collection.count()}")
+
+    return collection
+
+def retrieve_relevant_documents(user_question, collection, client):
+    result = client.models.embed_content(
+        model="gemini-embedding-2",
+        contents=user_question,
+        config=types.EmbedContentConfig(output_dimensionality=768)
+    )
+
+    query_embedding = result.embeddings[0].values
+    
+    rag_results = collection.query(
+        query_embeddings=[query_embedding],
+        n_results=3,
+        include=["documents", "metadatas", "distances"]
+    )
+
+    return rag_results
