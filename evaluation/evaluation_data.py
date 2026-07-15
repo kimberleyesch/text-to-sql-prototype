8f68b112861d9f83ce85192b69304a0657ec84ab
Kimberley <kimberley.esch@gmail.com>
2026-07-15 09:04:01 +0200

    Save LLM results in seperate folders depending on test approach

--- evaluation/evaluation_data.py
+++ evaluation/evaluation_data.py
@@ -4,6 +4,8 @@ from pathlib import Path
 EVALUATION_DIR = Path(__file__).resolve().parent
 QUESTIONS_PATH = EVALUATION_DIR / "test_questions.xlsx"
 RESULTS_PATH = EVALUATION_DIR / "results"
+RAG_PATH = RESULTS_PATH / "with_rag"
+WITHOUT_RAG_PATH = RESULTS_PATH / "without_rag"
 
 
 def get_questions():
@@ -24,6 +26,8 @@ def save_result(question_id, column_names, results, sql_query):
     """Save the query result in new CSV file"""
 
     RESULTS_PATH.mkdir(exist_ok=True)
+    RAG_PATH.mkdir(exist_ok=True)
+    WITHOUT_RAG_PATH.mkdir(exist_ok=True)
 
     df = pd.DataFrame(
         results,
@@ -39,14 +43,20 @@ def save_result(question_id, column_names, results, sql_query):
               sep=";",
               encoding="utf-8")
 
-def get_next_result_path(id):
-    """Return the next available file path name for the question"""
+def get_next_result_path(question_id):
+    """Return the next available file path name for the question."""
+
     counter = 1
 
-    path_name = RESULTS_PATH / f"{id}_result_{counter}.csv"
+    if "rag" in question_id.lower():
+        target_dir = RAG_PATH 
+    else:
+        target_dir = WITHOUT_RAG_PATH
 
+    path_name = (target_dir /  f"{question_id}_result_{counter}.csv")
+    
     while path_name.exists():
         counter += 1
-        path_name = RESULTS_PATH / f"{id}_result_{counter}.csv"
+        path_name = target_dir / f"{question_id}_result_{counter}.csv"
         
     return path_name
